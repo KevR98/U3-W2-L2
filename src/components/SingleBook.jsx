fb0c21dbf3513a1ac9b7337e3d564cffb066adbe
KevR98 <ramil.kevin98@gmail.com>
2025-07-29 17:22:05 +0200

    change the props in singlebook

--- src/components/SingleBook.jsx
+++ src/components/SingleBook.jsx
@@ -1,7 +1,8 @@
 import { Card } from 'react-bootstrap';
 // import CommentArea from './CommentArea'
 
-const SingleBook = (props) => {
+// destrutturo props scrivendolo in questo modo
+const SingleBook = ({ changeSelectedBook, book, selectedBook }) => {
   // state = {
   //   selected: false,
   // }
@@ -10,15 +11,14 @@ const SingleBook = (props) => {
     <>
       <Card
         // onClick={() => this.setState({ selected: !this.state.selected })}
-        onClick={() => props.changeSelectedBook(props.book.asin)}
+        onClick={() => changeSelectedBook(book.asin)}
         style={{
-          border:
-            props.selectedBook === props.book.asin ? '3px solid red' : 'none',
+          border: selectedBook === book.asin ? '3px solid red' : 'none',
         }}
       >
-        <Card.Img variant='top' src={props.book.img} />
+        <Card.Img variant='top' src={book.img} />
         <Card.Body>
-          <Card.Title style={{ color: 'black' }}>{props.book.title}</Card.Title>
+          <Card.Title style={{ color: 'black' }}>{book.title}</Card.Title>
         </Card.Body>
       </Card>
       {/* {this.state.selected && <CommentArea asin={this.props.book.asin} />} */}
